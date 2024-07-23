862b390f86e64dba7afce831bc81b13b00504606
heeeione <heewon410@naver.com>
2024-07-23 17:14:06 +0900

    feat: request algorithm course

--- src/components/CategoryBtn.js
+++ src/components/CategoryBtn.js
@@ -1,10 +1,21 @@
 import { CATEGORIES } from '../utils/categories';
+import { getAlgorithmCourse } from '../axios/openai';
 
 const CategoryBtn = () => {
+  const getCourse = (category) => {
+    getAlgorithmCourse(category).then((res) => {
+      console.log(res.data);
+    });
+  };
+
   return (
     <div>
       {CATEGORIES.map((category, idx) => {
-        return <button key={idx}>{category}</button>;
+        return (
+          <button key={idx} onClick={() => getCourse(category)}>
+            {category}
+          </button>
+        );
       })}
     </div>
   );
