4168376089c1e0ab76df891af7574dccc11a7205
heeeione <heewon410@naver.com>
2024-09-22 16:02:27 +0900

    :construction: wip: Implements battle page

---
+++ src/axios/battle.js
@@ -0,0 +1,3 @@
+export const joinBattle = () => {};
+
+export const checkMatchingStatus = () => {};
--- src/components/BattleBtn.js
+++ src/components/BattleBtn.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import { Box, Button, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { checkMatchingStatus, joinBattle } from '../axios/battle';
 
 const BattleBtn = () => {
   const [isWaiting, setIsWaiting] = React.useState(false);
+  const [matchId, setMatchId] = React.useState(null);
+  const navigate = useNavigate();
 
-  const handleJoinBattle = () => {
+  const handleJoinBattle = async () => {
     setIsWaiting(true);
+    try {
+      const response = await joinBattle();
+      setMatchId(response.matchId);
+    } catch (error) {
+      console.error('배틀 참여 중 오류가 발생했습니다:', error);
+      setIsWaiting(false);
+    }
   };
+
+  React.useEffect(() => {
+    let interval;
+    if (isWaiting && matchId) {
+      interval = setInterval(async () => {
+        try {
+          const status = await checkMatchingStatus(matchId);
+          if (status.isMatched) {
+            setIsWaiting(false);
+            navigate(`/battle/${matchId}`);
+          }
+        } catch (error) {
+          alert(error);
+          setIsWaiting(false);
+        }
+      }, 2000);
+    }
+    return () => clearInterval(interval);
+  }, [isWaiting, matchId]);
 
   return (
     <Box>
