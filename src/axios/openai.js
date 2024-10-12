a6bdc9d19319557a93c5b6491f2cbd7eb40d07cd
채희원 <68272931+heeeione@users.noreply.github.com>
2024-10-12 15:54:30 +0900

    refactor: Update openai recommendation problems api

--- src/axios/openai.js
+++ src/axios/openai.js
@@ -1,7 +1,7 @@
 import request from './axios';
 
 export const getAlgorithmCourse = async (category) => {
-  return request.post('/openai/recommend', { category });
+  return request.post('/openai/recommendProblems', { category });
 };
 
 export const getGrading = async (data) => {
