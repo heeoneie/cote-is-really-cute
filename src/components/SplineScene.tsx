d45a8fdeebe3fc947f896270ee15cd4044d0fcb3
heeeione <heewon410@naver.com>
2025-03-25 18:16:30 +0900

    refactor: Migration SplineScene to TS

--- src/components/SplineScene.tsx
+++ src/components/SplineScene.tsx
@@ -1,15 +1,22 @@
 import React, { Suspense } from 'react';
 import { items } from '@utils/items';
 
+interface Items {
+  item: string[];
+  item2: string[];
+  item3: string[];
+}
+interface SplineSceneProps {
+  level: number;
+  type: keyof Items;
+}
+
 const Spline = React.lazy(() => import('@splinetool/react-spline'));
 
-const SplineScene = ({ level, type }) => {
-  const getScene = (level, type) => {
-    if (type === 'item2') {
-      return items.item2[level - 4];
-    } else if (type === 'item3') {
-      return items.item3[level - 6];
-    }
+const SplineScene: React.FC<SplineSceneProps> = ({ level, type }) => {
+  const getScene = (level: number, type: keyof Items): string => {
+    if (type === 'item2') return items.item2[level - 4];
+    else if (type === 'item3') return items.item3[level - 6];
     return items[type][level - 1];
   };
 
