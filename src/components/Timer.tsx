2b7b6555ce9c94690a78f16e4c373fcebf4bbea9
heeeione <heewon410@naver.com>
2025-03-25 18:17:28 +0900

    refactor: Migration timer to TS and Declare import mp3

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import Countdown from 'react-countdown';
+import React, { useState, useEffect } from 'react';
+import Countdown, { CountdownRenderProps } from 'react-countdown';
 import { Box } from '@mui/material';
 import iphone_alarm from '../assets/iphone_alarm.mp3';
 import '../styles/Timer.css';
 
-const Timer = ({ initialMinutes }) => {
-  const [timerMinutes, setTimerMinutes] = React.useState(
+interface TimerProps {
+  initialMinutes: number;
+}
+
+const Timer: React.FC<TimerProps> = ({ initialMinutes }) => {
+  const [timerMinutes, setTimerMinutes] = useState<number>(
     initialMinutes * 60 * 1000,
   );
-  const [isTimerActive, setIsTimerActive] = React.useState(false);
-  const [isAlarmActive, setIsAlarmActive] = React.useState(false);
-  const [audio] = React.useState(new Audio(iphone_alarm));
+  const [isTimerActive, setIsTimerActive] = useState<boolean>(false);
+  const [isAlarmActive, setIsAlarmActive] = useState<boolean>(false);
+  const [audio] = useState<HTMLAudioElement>(new Audio(iphone_alarm));
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (isAlarmActive) audio.play();
     else {
       audio.pause();
@@ -20,24 +24,29 @@ const Timer = ({ initialMinutes }) => {
     }
   }, [isAlarmActive, audio]);
 
-  const startTimer = () => {
+  const startTimer = (): void => {
     setIsTimerActive(true);
     setIsAlarmActive(false);
   };
 
-  const stopTimer = () => {
+  const stopTimer = (): void => {
     setIsTimerActive(false);
     setIsAlarmActive(false);
     audio.pause();
     audio.currentTime = 0;
   };
 
-  const adjustTimer = (adjustment) =>
+  const adjustTimer = (adjustment: number): void => {
     setTimerMinutes((prev) =>
       Math.max(10 * 60 * 1000, prev + adjustment * 60 * 1000),
     );
+  };
 
-  const renderer = ({ hours, minutes, seconds }) => {
+  const renderer = ({
+    hours,
+    minutes,
+    seconds,
+  }: CountdownRenderProps): JSX.Element => {
     const formattedHours = String(hours).padStart(2, '0');
     const formattedMinutes = String(minutes).padStart(2, '0');
     const formattedSeconds = String(seconds).padStart(2, '0');
@@ -75,6 +84,7 @@ const Timer = ({ initialMinutes }) => {
           +10분
         </button>
       </Box>
+
       {!isTimerActive && (
         <h6 className="t-num">
           {String(Math.floor(timerMinutes / 3600000)).padStart(2, '0')}:
