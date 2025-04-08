5782efc0de0098f541a37f696887abad304ac99f
heeeione <heewon410@naver.com>
2025-04-08 13:38:09 +0900

    refactor: Add spline scene exception

--- src/components/SplineScene.tsx
+++ src/components/SplineScene.tsx
@@ -15,9 +15,18 @@ const Spline = React.lazy(() => import('@splinetool/react-spline'));
 
 const SplineScene: React.FC<SplineSceneProps> = ({ level, type }) => {
   const getScene = (level: number, type: keyof Items): string => {
-    if (type === 'item2') return items.item2[level - 4];
-    else if (type === 'item3') return items.item3[level - 6];
-    return items[type][level - 1];
+    try {
+      if (level <= 0) {
+        console.error('레벨은 0보다 커야 합니다');
+        return '';
+      }
+      if (type === 'item2') return items.item2[level - 4];
+      else if (type === 'item3') return items.item3[level - 6];
+      return items[type][level - 1];
+    } catch (error) {
+      console.error('장면을 가져오는 중 오류 발생:', error);
+      return '';
+    }
   };
 
   return (
