ee8a821b6880e6093707dfae604fa853785c4b30
heeeione <heewon410@naver.com>
2025-04-19 14:39:42 +0900

    init: Add dark mode config

---
+++ src/components/system/ThemeInitializer.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { useEffect } from 'react';
+
+export default function ThemeInitializer() {
+    useEffect(() => {
+        const systemDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+        if (systemDark) document.documentElement.classList.add('dark');
+        else document.documentElement.classList.remove('dark');
+
+    }, []);
+
+    return null;
+}
